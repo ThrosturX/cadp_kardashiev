f0c8c3084a11f6820fe74657e65152f5d2d7a764
Jón Reginbald <joni12@ru.is>
2015-05-12 10:06:25 +0000

    record for resources

--- src/solar_system.erl
+++ src/solar_system.erl
@@ -1,8 +1,11 @@
 -module(solar_system).
 -export([start/0, home_planet/0]).
 
+-record(resource, {iron = 0, food = 0, livestock = 0}).
+
 start() -> 
-	register(home, spawn(solar_system, home_planet, [])).
+	register(home, spawn(solar_system, home_planet, [])).i
+	
 
 home_planet() -> io:format("Home planet").
 
