d9d1dfd8fd61be6bb4129ef72935849b14161f0b
Jón Reginbald <joni12@ru.is>
2015-05-12 11:22:18 +0000

    harvest

--- src/solar_system.erl
+++ src/solar_system.erl
@@ -3,34 +3,68 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	terminate/2, code_change/3]).
--export([start_link/0, home_planet/0, spawner/0]).
+-export([start_link/0, home_planet/0, spawner/0, print_resources/0, harvest/0, harvesting/0]).
 
 -define(SERVER, ?MODULE).
 
--record(resource, {iron = 0, food = 0, livestock = 0}).
+-record(resources, {iron = 0, food = 0, livestock = 0}).
+
+sleep(T) ->
+	receive
+	after T -> true
+	end.
+
+randomSleep(T) ->
+	sleep(random:uniform(T)).
 
 start_link() ->
+	<<A:32, B:32, C:32>> = crypto:rand_bytes(12),
+	random:seed(A,B,C),
 	register(solar, self()),
 	register(home, spawn(solar_system, home_planet, [])),
 	spawn(solar_system, spawner, []),
 	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-init([]) -> 
+init([]) -> 	
+	{ok, {#resources{}, 0}}.
+
+home_planet() -> 
+	io:format("Home planet~n").
+
+print_resources() ->
+	gen_server:call(solar_system, resources).
+
+harvest() ->
+	io:format("Harvest~n"),
+	spawn(solar_system, harvesting, []).
 	
-	{ok, []}.
+harvesting() ->
+	io:format("Harvesting~n"),
+	randomSleep(2000),
+	gen_server:cast(solar_system, {harvest, 10, 0, 0}).	
 
-home_planet() -> io:format("Home planet~n").
+spawner() -> 
+	io:format("Spawner~n").
 
-spawner() -> io:format("Spawner~n").
-
+handle_call(resources, _From, State) ->
+	{Resources, _} = State,
+	io:format("Resources: ~p~n", [Resources]),
+	{reply, [], State};
 handle_call(_Msg, _From, State) ->
 	{reply, [], State}.
 
+handle_cast({harvest, Iron, Food, Livestock}, State) ->
+	io:format("harvest cast~n"),
+	{Resources, Rest} = State,
+	A = Resources#resources.iron,
+	B = Resources#resources.food,
+	C = Resources#resources.livestock,
+	{noreply, {#resources{iron = Iron+A, food = Food+B, livestock = Livestock+C}, Rest}};	
 handle_cast(stop, State) ->
 	{stop, normal, State}.
 
 handle_info(Info, State) ->
-	io:format("~p~n", Info),
+	io:format("~p~n", [Info]),
 	{noreply, State}.
 
 terminate(normal, _State) ->
