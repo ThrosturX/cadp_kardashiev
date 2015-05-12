1217e2c1df1888edf5602af80d9eaee29aa1d0b4
Jón Reginbald <joni12@ru.is>
2015-05-12 10:13:03 +0000

    registering solar system and start_link

--- src/solar_system.erl
+++ src/solar_system.erl
@@ -1,11 +1,17 @@
 -module(solar_system).
+-behavior(gen_server).
+
 -export([start/0, home_planet/0]).
+
+-define(SERVER, ?MODULE).
 
 -record(resource, {iron = 0, food = 0, livestock = 0}).
 
-start() -> 
-	register(home, spawn(solar_system, home_planet, [])).i
-	
+start() ->
+	register(solar, self()),
+	register(home, spawn(solar_system, home_planet, [])),
+	spawn(solar_system, spawner, []),
+	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 home_planet() -> io:format("Home planet").
 
