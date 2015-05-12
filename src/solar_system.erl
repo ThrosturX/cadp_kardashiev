5f773c46072c2014a0bd209e74411ae8f83f6264
Jón Reginbald <joni12@ru.is>
2015-05-12 10:32:34 +0000

    handle functions added

--- src/solar_system.erl
+++ src/solar_system.erl
@@ -1,17 +1,41 @@
 -module(solar_system).
 -behavior(gen_server).
 
--export([start/0, home_planet/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	terminate/2, code_change/3]).
+-export([start_link/0, home_planet/0, spawner/0]).
 
 -define(SERVER, ?MODULE).
 
 -record(resource, {iron = 0, food = 0, livestock = 0}).
 
-start() ->
+start_link() ->
 	register(solar, self()),
 	register(home, spawn(solar_system, home_planet, [])),
 	spawn(solar_system, spawner, []),
 	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-home_planet() -> io:format("Home planet").
+init([]) -> 
+	
+	{ok, []}.
+
+home_planet() -> io:format("Home planet~n").
+
+spawner() -> io:format("Spawner~n").
+
+handle_call(_Msg, _From, State) ->
+	{reply, [], State}.
+
+handle_cast(stop, State) ->
+	{stop, normal, State}.
+
+handle_info(Info, State) ->
+	io:format("~p~n", Info),
+	{noreply, State}.
+
+terminate(normal, _State) ->
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
 
