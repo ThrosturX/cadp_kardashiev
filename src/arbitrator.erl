ce1a723c5acebe63fee7fd6cff985cbbb76adbee
Björn Ingi Baldvinsson <bjornb13@ru.is>
2015-05-13 16:46:12 +0000

    call the right function

--- src/arbitrator.erl
+++ src/arbitrator.erl
@@ -42,7 +42,9 @@ die() -> client:notify(die).
 %%%% GUI to Solar System
 %% Connect to network of nodes
 connect(Node) -> solar_system:connect(l2a(Node)).
-set_node_name(Node) -> solar_system:connect(l2a(Node)).
+set_node_name(Node) -> 
+	%io:format("Arbitrator set_node_name: ~p~n", [Node]),
+	solar_system:set_node_name(l2a(Node)).
 
 %%% Trade Section
 %% Send to all nodes request trade  
