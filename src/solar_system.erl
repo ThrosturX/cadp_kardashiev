294db2a4b57f85b0ab0333052a00ed653d8fe464
Björn Ingi Baldvinsson <bjornb13@ru.is>
2015-05-13 21:02:38 +0000

    Contact list added to State

--- src/solar_system.erl
+++ src/solar_system.erl
@@ -282,20 +282,23 @@ init([]) ->
 	% TradeRes: resources reserved for active offers
 	% Requests: Trade requests from other nodes
 	% Offers: Offers from other nodes
+	% OutOffers: Offers to other nodes
+	% Contacts: Nodes we have made contact with
 	Resources = dict:from_list([{'Iron', 10}, {'Food', 10}, {'Gas', 10}]),
 	Ships = dict:from_list([{'Cargo ship', 3}, {'Harvester', 3}, {'Escort', 3}]),
 	TradeRes = dict:from_list([{'Iron', 0}, {'Food', 0}, {'Gas', 0}]),
 	Requests = dict:from_list([]),
 	Offers = dict:from_list([]),
 	OutOffers = dict:from_list([]),
+	Contacts = dict:from_list([]),
 	arbitrator:update_ships(dict:to_list(Ships)),
 	arbitrator:update_resources(dict:to_list(Resources)),
-	{ok, {Resources, Ships, TradeRes, Requests, Offers, OutOffers}}.
+	{ok, {Resources, Ships, TradeRes, Requests, Offers, OutOffers, Contacts}}.
 
 %% checks if there are enough resources if so detract from resources
 %% and reply with ok to build else reply with don't build
 handle_call({build, Iron, Food, Gas}, _From, State) ->
-	{Res, Ships, Trade, Req, Off, Out} = State,
+	{Res, Ships, Trade, Req, Off, Out, Con} = State,
 	I = dict:fetch('Iron', Res),
 	F = dict:fetch('Food', Res),
 	G = dict:fetch('Gas', Res),
@@ -306,24 +309,25 @@ handle_call({build, Iron, Food, Gas}, _From, State) ->
 
 			NewRes = dict:update_counter('Gas', -Gas, TempRes2),
 			arbitrator:update_resources(dict:to_list(NewRes)),
-			{reply, build_ok, {NewRes, Ships, Trade, Req, Off, Out}};	
+			{reply, build_ok, {NewRes, Ships, Trade, Req, Off, Out, Con}};	
 		true ->
 			{reply, build_nores, State}
 	end;	
 %% prints the resources and ships available
 handle_call(resources, _From, State) ->
-	{Resources, Ships, TradeRes, Req, Off, Out} = State,
+	{Resources, Ships, TradeRes, Req, Off, Out, Con} = State,
 	io:format("Resources: ~p~n", [dict:to_list(Resources)]),
 	io:format("Ships: ~p~n", [dict:to_list(Ships)]),
 	io:format("TradeRes: ~p~n", [dict:to_list(TradeRes)]),
 	io:format("Trade requests: ~p~n", [dict:to_list(Req)]),
 	io:format("Trade offers: ~p~n", [dict:to_list(Off)]),
 	io:format("Outgoing trade offers: ~p~n", [dict:to_list(Out)]),
+	io:format("Contacts: ~p~n", [dict:to_list(Con)]),
 	{reply, [], State};
 %% starts a harvest and reserves a harvester if one is available. If not it ends the operation
 handle_call(start_harvest, _From, State) ->			
 	io:format("check if enough ships~n"),
-	{Res, Ships, Trade, Req, Off, Out} = State,
+	{Res, Ships, Trade, Req, Off, Out, Con} = State,
 	H = dict:fetch('Harvester', Ships),
 	if 
 		H == 0 -> 
@@ -331,12 +335,12 @@ handle_call(start_harvest, _From, State) ->
 		true -> 
 			NewShips = dict:update_counter('Harvester', -1, Ships),
 			arbitrator:update_ships(dict:to_list(NewShips)),
-			{reply, ship, {Res, NewShips, Trade, Req, Off, Out}}
+			{reply, ship, {Res, NewShips, Trade, Req, Off, Out, Con}}
 	end;
 %% checks if the resources and ships needed for the given trade is available
 handle_call({reserve_resource, Type, Qty}, _From, State) ->
 	io:format("Check if enough resources~n"),
-	{Res, Ships, Trade, Req, Off, Out} = State,
+	{Res, Ships, Trade, Req, Off, Out, Con} = State,
 	C = dict:fetch('Cargo ship', Ships),
 	if 
 		C == 0 -> 
@@ -350,23 +354,23 @@ handle_call({reserve_resource, Type, Qty}, _From, State) ->
 					NewTrade = dict:update_counter(Type, Qty, Trade),
 					arbitrator:update_resources(dict:to_list(NewRes)),
 					arbitrator:update_ships(dict:to_list(NewShips)),
-					{reply, ok, {NewRes, NewShips, NewTrade, Req, Off, Out}};
+					{reply, ok, {NewRes, NewShips, NewTrade, Req, Off, Out, Con}};
 				true -> 
 					{reply, nores, State}
 			end
 	end;
 handle_call({get_offer_from, Node}, _From, State) ->
-	{_, _, _, _, Off, _} = State,
+	{_, _, _, _, Off, _, _} = State,
 	[Offer] = dict:fetch(Node, Off),
 	{reply, Offer, State};
 handle_call({have_offer_to, Node}, _From, State) ->
-	{_, _, _, _, _, Out} = State,
+	{_, _, _, _, _, Out, _} = State,
 	Reply = dict:is_key(Node, Out),
 	%io:format("Reply: ~p~n", [Reply]),
 	{reply, Reply, State};
 handle_call({Node, accept_offer, _Msg}, _From, State) ->
 	%% Check if the key Node exists in out offers, if so confirm trade, otherwise cancel
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	ContainsNode = dict:is_key(Node, Out),
 	if
 		ContainsNode == true ->
@@ -377,13 +381,13 @@ handle_call({Node, accept_offer, _Msg}, _From, State) ->
 			NewShips = dict:update_counter('Cargo ship', 1, Ships),
 			NewRes = dict:update_counter(TGot, QG, Res),
 			NewTradeRes = dict:update_counter(THad, -QH, TradeRes),
-			{reply, confirm, {NewRes, NewShips, NewTradeRes, Req, Off, NewOut}};
+			{reply, confirm, {NewRes, NewShips, NewTradeRes, Req, Off, NewOut, Con}};
 		true ->
 			{reply, cancel, State}
 	end;
 handle_call(get_contacts, _From, State) ->
-	{_, _, _, Req, _, _} = State,
-	Contacts = dict:fetch_keys(Req),
+	{_, _, _, _, _, _, Con} = State,
+	Contacts = dict:fetch_keys(Con),
 	{reply, Contacts, State};
 handle_call(_Msg, _From, State) ->
 	{reply, [], State}.
@@ -392,74 +396,78 @@ handle_call(_Msg, _From, State) ->
 %% Builds ship of type Type and adds it to Ships, takes random time
 handle_cast({building, Type}, State) ->
 	%io:format("Cast-building: ~w~n", [Type]),
-	{Resources, Ships, Trade, Req, Off, Out} = State,
+	{Resources, Ships, Trade, Req, Off, Out, Con} = State,
 	NewShips = dict:update_counter(Type, 1, Ships),
 	arbitrator:update_ships(dict:to_list(NewShips)),
 	%io:format("Cast-building: ~w - Done!~n", [Type]),
-	{noreply, {Resources, NewShips, Trade, Req, Off, Out}};
+	{noreply, {Resources, NewShips, Trade, Req, Off, Out, Con}};
 %% ends the harvest and increases our current resources accordingly
 handle_cast({harvest, Type, Qty}, State) ->
 	io:format("harvest cast~n"),
 	%io:format("State is: ~p~n", [State]),
-	{Resources, Ships, Trade, Req, Off, Out} = State,
+	{Resources, Ships, Trade, Req, Off, Out, Con} = State,
 	NewShips = dict:update_counter('Harvester', 1, Ships),
 	NewRes = dict:update_counter(Type, Qty, Resources),
 	arbitrator:update_ships(dict:to_list(NewShips)),
 	arbitrator:update_resources(dict:to_list(NewRes)),
 	io:format("~w: ~w~n", [Type, Qty]),
-	{noreply, {NewRes, NewShips, Trade, Req, Off, Out}};	
+	{noreply, {NewRes, NewShips, Trade, Req, Off, Out, Con}};	
 %% receives a message from another player
 handle_cast({Node, msg, Msg}, State) ->
+	{Resources, Ships, Trade, Req, Off, Out, Con} = State,
+	NewCon = dict:store(Node, 0, Con),
 	arbitrator:format("!!! Private message from ~w: ~p !!!~n", [Node, Msg]),
-	{noreply, State};
+	{noreply, {Resources, Ships, Trade, Req, Off, Out, NewCon}};
 %% receives a trade request from another player
 handle_cast({Node, rtrade, {TWant, THave}}, State) ->
 	io:format("Trade request from ~w: ~w, ~w~n", [Node, TWant, THave]),
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	Fun = fun(Old) -> [{TWant, THave}] ++ Old -- [{TWant, THave}] end,
-	NReq = dict:update(Node, Fun, [{TWant, THave}], Req),	
+	NReq = dict:update(Node, Fun, [{TWant, THave}], Req),
+	NewCon = dict:store(Node, 0, Con),		
 	arbitrator:update_contacts(NReq),
-	{noreply, {Res, Ships, TradeRes, NReq, Off, Out}};
+	{noreply, {Res, Ships, TradeRes, NReq, Off, Out, NewCon}};
 %% receives a trade cancellation from another player
 handle_cast({Node, ctrade, {TWant, THave}}, State) ->
 	io:format("Cancel request from ~w: ~w, ~w~n", [Node, TWant, THave]),
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	Fun = fun(Old) -> Old -- [{TWant, THave}] end,
 	NReq = dict:update(Node, Fun, [{TWant, THave}], Req),
 	arbitrator:update_contacts(NReq),
-	{noreply, {Res, Ships, TradeRes, NReq, Off, Out}};
+	{noreply, {Res, Ships, TradeRes, NReq, Off, Out, Con}};
 handle_cast({Node, offer, {TWant, QT, THave, QH}}, State) ->
 	io:format("Offer from ~w: ~wx~w for ~wx~w~n", [Node, TWant, QT, THave, QH]),
 	%TODO: Update offer list in GUI.
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	Fun = fun(Old) -> Old end,
 	NOff = dict:update(Node, Fun, [{TWant, QT, THave, QH}], Off),
 	arbitrator:update_offers(NOff),
-	{noreply, {Res, Ships, TradeRes, Req, NOff, Out}};
+	NewCon = dict:store(Node, 0, Con),
+	{noreply, {Res, Ships, TradeRes, Req, NOff, Out, NewCon}};
 %% receive an offer cancellation
 handle_cast({cOutOffer, Node}, State) ->
 	io:format("Cancel our own offer ~n"),
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	[{_, _, THave, Qt}] = dict:fetch(Node, Out),
 	NShips = dict:update_counter('Cargo ship', 1, Ships),
 	NOut = dict:erase(Node, Off),
 	NRes = dict:update_counter(THave, Qt, Res),
 	NTradeRes = dict:update_counter(THave, -Qt, TradeRes),
-	{noreply, {NRes, NShips, NTradeRes, Req, Off, NOut}};
+	{noreply, {NRes, NShips, NTradeRes, Req, Off, NOut, Con}};
 handle_cast({Node, coffer, _}, State) ->
 	io:format("Remove cancelled offer~n"),
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	NOff = dict:erase(Node, Off),
 	arbitrator:update_offers(NOff),
-	{noreply, {Res, Ships, TradeRes, Req, NOff, Out}};
+	{noreply, {Res, Ships, TradeRes, Req, NOff, Out, Con}};
 %% adds an outgoing offer to the list
 handle_cast({Node, outoffer, {TWant, QT, THave, QH}}, State) ->
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	Fun = fun(Old) -> Old end,
 	NOut = dict:update(Node, Fun, [{TWant, QT, THave, QH}], Out),
-	{noreply, {Res, Ships, TradeRes, Req, Off, NOut}};	
+	{noreply, {Res, Ships, TradeRes, Req, Off, NOut, Con}};	
 handle_cast({offer_confirmed, Node}, State) ->
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	[{THad, QH, TGot, QG}] = dict:fetch(Node, Off),
 	
 	%% Update dictionaries
@@ -468,9 +476,9 @@ handle_cast({offer_confirmed, Node}, State) ->
 	NewRes = dict:update_counter(TGot, QG, Res),
 	NewTradeRes = dict:update_counter(THad, -QH, TradeRes),
 	
-	{noreply, {NewRes, NewShips, NewTradeRes, Req, NewOff, Out}};
+	{noreply, {NewRes, NewShips, NewTradeRes, Req, NewOff, Out, Con}};
 handle_cast({offer_cancelled, Node}, State) ->
-	{Res, Ships, TradeRes, Req, Off, Out} = State,
+	{Res, Ships, TradeRes, Req, Off, Out, Con} = State,
 	[{THad, QH, _, _}] = dict:fetch(Node, Off),
 	
 	%% Update dictionaries
@@ -479,7 +487,7 @@ handle_cast({offer_cancelled, Node}, State) ->
 	NewRes = dict:update_counter(THad, QH, Res),
 	NewTradeRes = dict:update_counter(THad, -QH, TradeRes),
 	
-	{noreply, {NewRes, NewShips, NewTradeRes, Req, NewOff, Out}};
+	{noreply, {NewRes, NewShips, NewTradeRes, Req, NewOff, Out, Con}};
 handle_cast(stop, State) ->
 	io:format("Stopping solar_system ~n"),
 	{stop, normal, State}.
